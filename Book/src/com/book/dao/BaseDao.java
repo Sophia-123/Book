af33637ff1a8443cb43dbe9f2f600623e075fa9d
sophia <3427527804@qq.com>
2021-06-03 20:54:51 +0800

    添加了实体类，将表转化成实体类

--- Book/src/com/book/dao/BaseDao.java
+++ Book/src/com/book/dao/BaseDao.java
@@ -3,8 +3,12 @@ package com.book.dao;
 import java.util.List;
 
 import com.book.util.JdbcTemplate;
-
+//只声明方法，不声明类型，---》范型
+//E:entity,表示实体类的类型
+//I:id,表示实体类的id类型
+//范型不能使用基本类型，所以在定义的时候要使用包装类型
 public interface BaseDao<E, I> {
+	//
 	void insert(E entity);
 
 	void update(E entity);
